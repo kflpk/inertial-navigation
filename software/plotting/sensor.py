63e063294274cf1a563d62a7e25c6b64f733cf79
Yoshua-chan <31251884+Yoshua-chan@users.noreply.github.com>
2023-05-21 17:48:51 +0200

    added a mockup sensor class and more plots

---
+++ software/plotting/sensor.py
@@ -0,0 +1,37 @@
+import time
+import numpy as np
+from scipy import signal
+
+class Sensor:
+    def __init__(self):
+        self._initial_time = time.time()
+        self.sampling_rate = 10
+
+    def get_acc(self):
+        now = time.time()
+        delta_t = now - self._initial_time
+        ax = 10 * np.sin(delta_t - 2 * np.pi / 3)
+        ay = 10 * np.sin(delta_t - 4 * np.pi / 3)
+        az = 10 * np.sin(delta_t)
+        return delta_t, ax, ay, az
+
+    def get_mag(self):
+        now = time.time()
+        delta_t = now - self._initial_time
+        mx = 10 * signal.sawtooth(delta_t - 2 * np.pi / 3, 0.5)
+        my = 10 * signal.sawtooth(delta_t - 4 * np.pi / 3, 0.5)
+        mz = 10 * signal.sawtooth(delta_t                , 0.5)
+        return delta_t, mx, my, mz
+        
+
+    def gyro(self):
+        now = time.time()
+        delta_t = now - self._initial_time
+        gx = 10 * signal.sawtooth(delta_t - 2 * np.pi / 3, 1)
+        gy = 10 * signal.sawtooth(delta_t - 4 * np.pi / 3, 1)
+        gz = 10 * signal.sawtooth(delta_t                , 1)
+        return delta_t, gx, gy, gz
+
+if __name__ == "__main__":
+    sensor = Sensor()
+    sensor.get_acc()
--- software/plotting/update_quiver.py
+++ software/plotting/update_quiver.py
@@ -1,13 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from sensor import *
 
 t = np.arange(0, 100, 0.01);
-x = 15*np.cos(t)
-y = 15*np.sin(t)
-z = 15*np.tan(t)
-_sample_no = -1
+x = 15 * np.cos(10*t)
+y = 15 * np.cos(10*t - 2 * np.pi / 3)
+z = 15 * np.cos(10*t - 4 * np.pi / 3)
+_sample_no  = -1
 _sample_no2 = -1
+
+t  = [ ]
+ax = [ ]
+ay = [ ]
+az = [ ]
+mx, my, mz = [], [], []
+mt = []
+
+sensor = Sensor()
 
 def get_acc():
     global _sample_no
@@ -17,7 +27,7 @@ def get_acc():
 def get_acc2():
     global _sample_no2
     _sample_no2 += 1
-    return x[_sample_no2], y[_sample_no2], z[_sample_no2]
+    return _sample_no2, x[_sample_no2], y[_sample_no2], z[_sample_no2]
 
 def update_3d(i):
     acc_ax.cla()
@@ -31,17 +41,102 @@ def update_3d(i):
     acc_ax.set_ylim(-15, 15)
     acc_ax.set_zlim(-15, 15)
 
-    print(i)
     acc_ax.set_xlabel("X")
     acc_ax.set_ylabel("Y")
     acc_ax.set_zlabel("Z")
     acc_ax.set_title("siema")
 
+def update_2d(i):
+    global ax, ay, az
+
+    acc_2d.cla()
+    acc_2d.grid(True)
+    acc_2d.set_ylim(-20, 20)
+    ts, x, y, z = sensor.get_acc()
+
+    ax.append(x)
+    ay.append(y)
+    az.append(z)
+    t.append(ts)
+
+    if len(ax) == 100:
+        ax.pop(0)
+        ay.pop(0)
+        az.pop(0)
+        t.pop(0)
+
+    acc_2d.plot(t, ax)
+    acc_2d.plot(t, ay)
+    acc_2d.plot(t, az)
+
+    acc_2d.set_xlabel("Time")
+    acc_2d.set_ylabel("Acc")
+    acc_2d.set_title("Accelerometer")
+
+    # ========== MAGNETOMETER =================
+
+    mag_2d.cla()
+    mag_2d.grid(True)
+    mag_2d.set_ylim(-20, 20)
+    ts, x, y, z = sensor.get_mag()
+
+    mx.append(x)
+    my.append(y)
+    mz.append(z)
+    mt.append(ts)
+
+    if len(mx) == 100:
+        mx.pop(0)
+        my.pop(0)
+        mz.pop(0)
+        mt.pop(0)
+
+    mag_2d.plot(mt, mx)
+    mag_2d.plot(mt, my)
+    mag_2d.plot(mt, mz)
+
+    mag_2d.set_xlabel("Time")
+    mag_2d.set_ylabel("Mag")
+    mag_2d.set_title("Magnetometer")
+
+
+def update_mag2d(i):
+    # global ax, ay, az
+
+    mag_2d.cla()
+    mag_2d.grid(True)
+    mag_2d.set_ylim(-20, 20)
+    ts, x, y, z = sensor.get_mag()
+
+    mx.append(x)
+    my.append(y)
+    mz.append(z)
+    mt.append(ts)
+
+    if len(mx) == 100:
+        mx.pop(0)
+        my.pop(0)
+        mz.pop(0)
+        mt.pop(0)
+
+    mag_2d.plot(mt, mx)
+    mag_2d.plot(mt, my)
+    mag_2d.plot(mt, mz)
+
+    mag_2d.set_xlabel("Time")
+    mag_2d.set_ylabel("Mag")
+    mag_2d.set_title("Magnetometer")
+
 fig = plt.figure()
 acc_ax = fig.add_subplot(1, 2, 1, projection="3d")
-vel_ax = fig.add_subplot(1, 2, 2)
+acc_2d = fig.add_subplot(2, 2, 2)
+mag_2d = fig.add_subplot(2, 2, 4)
 
 
 
-ani1 = FuncAnimation(fig, update_3d, interval=10)
+ani_acc3d, = FuncAnimation(fig, update_3d, interval=10, blit=True)
+ani_acc2d, = FuncAnimation(fig, update_2d, interval=10, blit=True)
+# ani_mag2d = FuncAnimation(fig, update_mag2d, interval=10)
+
+
 plt.show()
